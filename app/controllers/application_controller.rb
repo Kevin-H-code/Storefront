faf0878f82e5ca78e1618347361a482e4d1e0dc0
Kevin <swadon09@gmail.com>
2019-10-06 22:06:59 -0400

    devise started and basic navbar added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :company, :location, ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :company, :location, ])
+
 end
